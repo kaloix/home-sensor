a973fa82a4974d4fab8480f67076975c2f5b559a
Stefan Schindler <stefan@kaloix.de>
2015-09-08 13:36:39 +0200

    bugfix time config

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import datetime
 
 data_period = datetime.timedelta(days=1)
-summary_period = datetime.timedelta(years=1)
+summary_period = datetime.timedelta(days=365)
 client_interval = datetime.timedelta(minutes=10)
 server_interval = datetime.timedelta(minutes=1)
 warning_pause = datetime.timedelta(days=1)
