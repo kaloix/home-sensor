63ab7a045b5eafa3296d301097b5de91dcfa43c5
Stefan Schindler <stefan@kaloix.de>
2015-09-11 18:41:16 +0200

    change debug in thermosolar

--- measurement.py
+++ measurement.py
@@ -11,6 +11,7 @@ def w1_temp(file):
 			raise Exception('sensor says no')
 
 def thermosolar_ocr(file):
+	return random.randrange(23, 43), bool(random.randrange(0, 2)) # FIXME
 	def parse_segment(image):
 		scipy.misc.imsave('seven_segment.png', image)
 		command = ['ssocr/ssocr', '--number-digits=2', '--background=black', 'seven_segment.png']
@@ -26,5 +27,4 @@ def thermosolar_ocr(file):
 	seven_segment = image[top:top+height, left:left+width]
 	top, left, length = 146, 128, 15
 	pump_light = image[top:top+length, left:left+length]
-#	return parse_segment(seven_segment), parse_light(pump_light) # FIXME
-	return random.randrange(23, 43), bool(random.randrange(0, 2))
+	return parse_segment(seven_segment), parse_light(pump_light)
