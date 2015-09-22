21f9f1dea991d75b0561c0d7e25c3b318742f5ad
Stefan Schindler <stefan@kaloix.de>
2015-09-22 11:49:15 +0200

    merge presentation.py into server.py, more server.py constants

--- config.py
+++ config.py
@@ -5,12 +5,8 @@ summary_range = datetime.timedelta(days=365)
 sampling_interval = datetime.timedelta(minutes=1)
 transmit_interval = datetime.timedelta(minutes=10)
 allowed_downtime = 2 * transmit_interval
-server_interval = datetime.timedelta(minutes=1)
 warning_pause = datetime.timedelta(days=1)
-data_dir = 'data/'
-backup_dir = 'backup/'
 client_server = 'kaloix@adhara.uberspace.de:home-sensor/'
-web_dir = '/home/kaloix/html/sensor/'
 admin_address = 'stefan@kaloix.de'
 user_address = 'stefan@kaloix.de'
 enable_email = False
--- server.py
+++ server.py
@@ -9,10 +9,21 @@ import string
 import time
 import traceback
 
+import matplotlib.dates
+import matplotlib.pyplot
+import pysolar
+import pytz
+
 import config
 import notification
-import presentation
 import utility
+
+
+BACKUP_DIR = 'backup/'
+COLOR_CYCLE = ['b', 'g', 'r', 'c', 'm', 'y', 'k']
+DATA_DIR = 'data/'
+SERVER_INTERVAL = datetime.timedelta(minutes=1)
+WEB_DIR = '/home/kaloix/html/sensor/'
 
 
 def main():
@@ -52,7 +63,7 @@ def main():
 			break
 		logging.info('sleep, duration was {}s'.format(
 			round(time.time() - start)))
-		time.sleep(config.server_interval.total_seconds())
+		time.sleep(SERVER_INTERVAL.total_seconds())
 
 
 def loop(group, sensor_list, html_template):
@@ -61,33 +72,126 @@ def loop(group, sensor_list, html_template):
 	for s in sensor_list:
 		s.update()
 		s.check()
-	if os.system('cp {}{} {}'.format(config.data_dir, 'thermosolar.jpg', config.web_dir)):
+	if os.system('cp {}{} {}'.format(DATA_DIR, 'thermosolar.jpg', WEB_DIR)):
 		logging.error('cp thermosolar.jpg failed')
 
 	logging.info('write html')
 	html_filled = string.Template(html_template).substitute(
 		refresh_seconds = int(config.transmit_interval.total_seconds()),
 		group = group,
-		values = presentation.detail_html([s.history for s in sensor_list]),
+		values = detail_html([s.history for s in sensor_list]),
 		update_time = '{:%A %d. %B %Y %X}'.format(now),
 		year = '{:%Y}'.format(now))
-	with open(config.web_dir+group+'.html', mode='w') as html_file:
+	with open(WEB_DIR+group+'.html', mode='w') as html_file:
 		html_file.write(html_filled)
 
 	logging.info('generate plot')
-	presentation.plot_history([s.history for s in sensor_list], config.web_dir+group+'.png', now)
+	plot_history([s.history for s in sensor_list], WEB_DIR+group+'.png', now)
+
+
+def detail_html(histories):
+	string = list()
+	string.append('<ul>')
+	for history in histories:
+		string.append('<li>{}</li>'.format(history.html()))
+	string.append('</ul>')
+	return '\n'.join(string)
+
+
+def plot_history(history, file, now):
+	fig, ax = matplotlib.pyplot.subplots(figsize=(12, 4))
+	frame_start = now - config.detail_range
+	minimum, maximum = list(), list()
+	color_iter = iter(COLOR_CYCLE)
+	for h in history:
+		color = next(color_iter)
+		if hasattr(h, 'float') and h.float:
+			parts = list()
+			for measurement in h.float:
+				if not parts or measurement.timestamp - \
+						parts[-1][-1].timestamp > config.allowed_downtime:
+					parts.append(list())
+				parts[-1].append(measurement)
+			for index, part in enumerate(parts):
+				values, timestamps = zip(*part)
+				label = h.name if index == 0 else None
+				matplotlib.pyplot.plot(
+					timestamps, values, linewidth=3, color=color, label=label)
+			minimum.append(min(h.float.value)-1)
+			minimum.append(h.floor)
+			maximum.append(max(h.float.value)+1)
+			maximum.append(h.ceiling)
+		elif hasattr(h, 'boolean') and h.boolean:
+			for index, (start, end) in enumerate(prepare_bool_plot(h.boolean)):
+				label = h.name if index == 0 else None
+				matplotlib.pyplot.axvspan(
+					start, end, color=color ,alpha=0.5, label=label)
+	nights = int(config.detail_range / datetime.timedelta(days=1)) + 2
+	for index, (sunset, sunrise) in enumerate(nighttime(nights, now)):
+		label = 'Nacht' if index == 0 else None
+		matplotlib.pyplot.axvspan(
+			sunset, sunrise, color='black', alpha=0.2, label=label)
+	ax.xaxis.set_major_formatter(matplotlib.dates.DateFormatter('%H'))
+	ax.xaxis.set_major_locator(matplotlib.dates.HourLocator())
+	ax.yaxis.get_major_formatter().set_useOffset(False)
+	ax.yaxis.set_label_position('right')
+	ax.yaxis.tick_right()
+	matplotlib.pyplot.xlabel('Uhrzeit')
+	matplotlib.pyplot.xlim(frame_start, now)
+	matplotlib.pyplot.ylabel('Temperatur °C')
+	matplotlib.pyplot.ylim(min(minimum), max(maximum))
+	matplotlib.pyplot.grid(True)
+	matplotlib.pyplot.legend(
+		loc='lower left', bbox_to_anchor=(0, 1), borderaxespad=0, ncol=5,
+		frameon=False)
+	matplotlib.pyplot.savefig(filename=file, bbox_inches='tight')
+	matplotlib.pyplot.close()
+
+
+def nighttime(count, date_time):
+	# make aware
+	date_time = pytz.timezone('Europe/Berlin').localize(date_time)
+	# calculate nights
+	date_time -= datetime.timedelta(days=count)
+	sun_change = list()
+	for c in range(0, count+1):
+		date_time += datetime.timedelta(days=1)
+		sun_change.extend(pysolar.util.get_sunrise_sunset(
+			49.2, 11.08, date_time))
+	sun_change = sun_change[1:-1]
+	night = list()
+	for r in range(0, count):
+		night.append((sun_change[2*r], sun_change[2*r+1]))
+	# make naive
+	for sunset, sunrise in night:
+		yield sunset.replace(tzinfo=None), sunrise.replace(tzinfo=None)
+
+
+def prepare_bool_plot(boolean):
+	expect = True
+	for value, timestamp in boolean:
+		if value != expect:
+			continue
+		if expect:
+			start = timestamp
+			expect = False
+		else:
+			yield start, timestamp
+			expect = True
+	if not expect:
+		yield start, timestamp + config.allowed_downtime
 
 
 class Temperature(object):
 
 	def __init__(self, name, floor, ceiling):
 		self.history = utility.FloatHistory(name, floor, ceiling)
-		self.history.restore(config.backup_dir)
+		self.history.restore(BACKUP_DIR)
 		self.name = name
 
 	def update(self):
-		self.history.restore(config.data_dir)
-		self.history.backup(config.backup_dir)
+		self.history.restore(DATA_DIR)
+		self.history.backup(BACKUP_DIR)
 
 	def check(self):
 		pass # TODO
@@ -105,11 +209,11 @@ class Temperature(object):
 class Switch(object):
 	def __init__(self, name):
 		self.history = utility.BoolHistory(name)
-		self.history.restore(config.backup_dir)
+		self.history.restore(BACKUP_DIR)
 		self.name = name
 	def update(self):
-		self.history.restore(config.data_dir)
-		self.history.backup(config.backup_dir)
+		self.history.restore(DATA_DIR)
+		self.history.backup(BACKUP_DIR)
 	def check(self):
 		pass
 
