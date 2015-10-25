fa9e13c9a8670f077db07e32394a2f66b858c87d
Stefan Schindler <stefan@kaloix.de>
2015-10-25 11:25:29 +0100

    bugfix aware utc timestamps

--- server.py
+++ server.py
@@ -127,9 +127,6 @@ def detail_html(series_list):
 
 
 def _nighttime(count, date_time):
-#	# make aware
-#	date_time = pytz.timezone('Europe/Berlin').localize(date_time)
-	# calculate nights
 	date_time -= datetime.timedelta(days=count)
 	sun_change = list()
 	for c in range(0, count+1):
@@ -137,12 +134,8 @@ def _nighttime(count, date_time):
 		sun_change.extend(pysolar.util.get_sunrise_sunset(
 			49.2, 11.08, date_time))
 	sun_change = sun_change[1:-1]
-	night = list()
 	for r in range(0, count):
-		night.append((sun_change[2*r], sun_change[2*r+1]))
-#	# make naive
-#	for sunset, sunrise in night:
-#		yield sunset.replace(tzinfo=None), sunrise.replace(tzinfo=None)
+		yield sun_change[2*r], sun_change[2*r+1]
 
 
 def _plot_records(series_list, days, now):
@@ -310,7 +303,8 @@ class Series(object):
 			with open(filename, newline='') as csv_file:
 				for row in csv.reader(csv_file):
 					record = Record(
-						datetime.datetime.fromtimestamp(int(row[0])),
+						datetime.datetime.fromtimestamp(
+							int(row[0]), tz=datetime.timezone.utc),
 						_universal_parser(row[1]))
 					self._append(record)
 					self._summarize(record)
